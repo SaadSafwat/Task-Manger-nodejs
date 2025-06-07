fb02652f481e72f0d88b6d4c42e6755cb3316154
SaadSafwat <saadsafwat507@gmail.com>
2025-06-07 18:39:25 +0300

    add filter tasks

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -1,3 +1,27 @@
+// filterTasks
+
+export const filterTasks = async (req,res) => {
+    try{
+        const {category,priority,status} = req.query;
+
+        const query ={
+            user_id: req.user._id,
+        }
+
+    if (category) query.category = category;
+    if (priority) query.priority = priority;
+    if (status) query.status = status;
+
+    const tasks = await taskModel.find(query);
+
+    res.status(200).json({count: tasks.length, tasks});
+
+    }catch(err){
+        res.status(500).json({message: "Error fetching tasks", error: err.message});
+    }
+}
+
+
 // searchTasks
 
 export const searchTasks = async (req,res) => {
@@ -16,7 +40,7 @@ export const searchTasks = async (req,res) => {
                 { category:{$regex:search, $option:"i"}}
             ],
         })
-        res.status(200).json({count: tasks.length, tasks})
+        res.status(200).json({count: tasks.length, tasks});
     }catch(err){
         res.status(500).json({message: "Search failed", error: err.message});
     }
